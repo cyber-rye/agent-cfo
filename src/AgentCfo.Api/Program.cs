8c33cc1679fd3f655198421255baeb10e8ef2159
cyber-rye <rye42@pm.me>
2026-06-28 22:16:47 +0000

    feat: auto-migrate on startup + CORS for port 5174
    - AppDbContext.Database.MigrateAsync() runs on startup
    - No need for manual dotnet ef database update
    - Added localhost:5174 to CORS origins

--- src/AgentCfo.Api/Program.cs
+++ src/AgentCfo.Api/Program.cs
@@ -2,6 +2,8 @@ using System.Text.Json.Serialization;
 using AgentCfo.Api.Controllers;
 using AgentCfo.Application;
 using AgentCfo.Infrastructure;
+using AgentCfo.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,13 +27,20 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("Dashboard", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:5174")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
 
 var app = builder.Build();
+
+// Auto-migrate on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 if (app.Environment.IsDevelopment())
 {
